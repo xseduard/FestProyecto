abef5f28d8f93b8a7cd5e983d9ea8ba01cbc196f
Eduard Ramos <xseduard@gmail.com>
2018-05-24 17:47:29 -0500

    sistema

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,11 +16,11 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+            <div class="panel panel-success">
+                <div class="panel-heading">Inicio de repositorio</div>
 
                 <div class="panel-body">
-                    Session iniciada!
+                    Hola!, {{ auth()->user()->nombres }} {{ auth()->user()->apellidos }}
                 </div>
             </div>
         </div>
